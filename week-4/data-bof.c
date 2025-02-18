4d51890210a838ba0125717bed9872e5d12c6fd9
SpiritSeal <yssaketh@gmail.com>
2025-02-18 16:40:24 -0500

    week 4 content

---
+++ week-4/data-bof.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    char input[10];
+    char password[10] = "GreyHat123";
+
+    printf("Enter the password: ");
+    gets(input);
+
+    printf("Input: %s\n", input);
+    printf("Password: %s\n", password);
+
+    if (strncmp(input, password, 10) == 0) {
+        printf("Access granted.\n");
+    } else {
+        printf("Access denied.\n");
+    }
+
+    return 0;
+}
